f6fc2ebb1197aec5aa0d55f3a4925d922bd8c632
raksup <puskarrim@gmail.com>
2021-02-02 14:37:48 +0545

    Added User and Post Table

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy #import for SQL Alchemy
+from flask_migrate import Migrate       #import for db migrations
 from config import Config
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 app.config.from_object(Config)
 
-from app import routes
+from app import routes, models
