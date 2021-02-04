a2802b007cb45f50e7d669575942d0579ed8d099
raksup <puskarrim@gmail.com>
2021-02-04 15:05:31 +0545

    Error Pages and Edit Profile Feature

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, EqualTo, Email, ValidationError
+from wtforms.fields.simple import TextAreaField
+from wtforms.validators import DataRequired, EqualTo, Email, Length, ValidationError
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -25,3 +26,18 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Select a different email.')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About Me', validators=[Length(min=0,max=140)])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+    
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=username.data).first()
+            if user is not None:
+                raise ValidationError('Please choose a different username')
--- config.py
+++ config.py
@@ -6,3 +6,10 @@ class Config(object):
     '''Setting up the database'''
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+ os.path.join(basedir,'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ''' Setting up the mail server '''
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_SERVER') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['puskarrim@gmail.com']
