a8b132d85a9ea8530c867d55ec36a53a8518fba1
raksup <puskarrim@gmail.com>
2021-02-08 12:00:59 +0545

    Ajax

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, flash, url_for, redirect, request, g
+from flask import render_template, flash, url_for, redirect, request, g,\
+                jsonify
 from werkzeug.urls import url_parse
 from datetime import datetime
 from app import app, db
@@ -8,6 +9,8 @@ from app.models import User, Post
 from flask_login import login_required, login_user, current_user, logout_user
 from app.email import send_password_reset_email
 from flask_babel import _, get_locale
+from guess_language import guess_language
+from app.translate import translate
 
 @app.before_request
 def before_request():
@@ -22,7 +25,10 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language)>5:
+            language = ''
+        post = Post(body=form.post.data, author=current_user, language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
@@ -37,7 +43,12 @@ def index():
                            posts=posts.items, next_url=next_url,
                            prev_url=prev_url)
 
-
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    return jsonify({'text': translate(request.form['text'],
+                                      request.form['source_language'],
+                                      request.form['dest_language'])})
 
 @app.route('/explore')
 @login_required
